15d12327940a5f68d2b8c733671a56f5cd3757f2
Adi Munawar <munawar.adi31@gmail.com>
2022-04-16 15:53:44 +0700

    Menemukan Fitur Tak Terduga

--- test/triangle_app_test.dart
+++ test/triangle_app_test.dart
@@ -11,20 +11,24 @@ void main() {
       expect(() => detectTriangle(1, 2, -2), throwsA(isA<Exception>()));
     });
 
+    test('Should throw error when side a tambah b kurang dari c', () {
+      expect(() => detectTriangle(4, 1, 2), throwsA(isA<Exception>()));
+      expect(() => detectTriangle(5, 1, 3), throwsA(isA<Exception>()));
+    });
     test('Should return "Segitiga sama sisi" when all side are equal', () {
       expect(detectTriangle(1, 1, 1), "Segitiga sama sisi");
-      expect(detectTriangle(1, 1, 2), isNot("Segitiga sama sisi"));
+      expect(detectTriangle(2, 2, 1), isNot("Segitiga sama sisi"));
     });
 
     test('Should return "Segitiga Sama Kaki" when only two sides are equal', () {
-      expect(detectTriangle(1, 1, 2), "Segitiga Sama Kaki");
-      expect(detectTriangle(1, 2, 1), "Segitiga Sama Kaki");
+      expect(detectTriangle(2, 2, 3), "Segitiga Sama Kaki");
+      expect(detectTriangle(4, 2, 4), "Segitiga Sama Kaki");
       expect(detectTriangle(1, 2, 2), "Segitiga Sama Kaki");
       expect(detectTriangle(4, 1, 4), "Segitiga Sama Kaki");
     });
 
     test('Should return "Segitiga Sembarang" when no sides are equal', () {
-      expect(detectTriangle(1, 2, 3), "Segitiga Sembarang");
+      expect(detectTriangle(2, 4, 3), "Segitiga Sembarang");
     });
   });
 }
